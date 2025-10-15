1c25b4a87c498fc035d6859241e3a91308648c27
s2230095 <s2230095@ed.ac.uk>
2025-10-15 20:59:47 +0100

    working on reading data

--- data_reader.cpp
+++ data_reader.cpp
@@ -1,7 +1,6 @@
-//this file is meant to read in data and store them in variables.
-//this data can then be passed into the Mesh3D class to create a mesh object.
 #include <fstream>
 #include <iostream>
+#include <sstream>
 
 int main(){
     std::ifstream inFile;
@@ -9,17 +8,37 @@ int main(){
     std::string points;
     std::string entire_passage;
 
-
+    // ask the user for the path of the file.
     std::cout << "path of file: ";
     std::cin >> file_path;
-    inFile.open(file_path);
 
-if (inFile.is_open()) {
-        while (std::getline(inFile, points)) {
-            entire_passage += points;
-        }
-        inFile.close();
-}
-        std::cout << entire_passage << std::endl;
+    // open the file and read it line by line and append into entire_passage
+        inFile.open(file_path);
+            if (inFile.is_open()) {
+                    while (std::getline(inFile, points)) {
+                        entire_passage += points;
+                    }
+                    inFile.close();
+            }
+    
+    std::cout << entire_passage << std::endl; // sanity check printing
+
+
+    // this essentially makes the string into a constant stream of info like std::cin
+    std::istringstream string_stream(entire_passage);
+    
+    // grab the first integer and store it.
+    int num_points;
+    string_stream >> num_points;
+    std::cout << num_points << '\n' << std::endl; // sanity check the num_points by printing.
+
+    // establish the pattern for the points:
+    double x, y, z;
+    char openPar, closePar;
+    
+    string_stream >> openPar >> x >> y >> z >> closePar;
+
+    std::cout << "x is:" << x << "y is:" << y << "z is:" << z << std::endl;
+
     return 0;
 };
--- points.h
+++ points.h
@@ -7,6 +7,16 @@ class mesh_points {
     public:
         void add_point(std::tuple<double, double, double> point){
             point_positions.push_back(point);
-        }
+        };
 
+        void set_num_points(int n){
+            num_points = n;
+        };
+
+        void print_points(){
+            for (std::tuple<double, double, double> point : point_positions){
+                double x, y, z = point;
+                std::cout << "(" << x << ", " << y << ", " << z << ")" << std::endl;
+            };
+        };
 };
