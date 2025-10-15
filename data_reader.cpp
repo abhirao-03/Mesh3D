a73c848ad03451e1b743324f9c228120a32c0f8d
s2230095 <s2230095@ed.ac.uk>
2025-10-15 12:52:31 +0100

    creating outline for project

---
+++ data_reader.cpp
@@ -0,0 +1,2 @@
+//this file is meant to read in data and store them in variables.
+//this data can then be passed into the Mesh3D class to create a mesh object.
---
+++ mesh.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+
+class Mesh3D {
+    private:
+    // read in points, faces, cells, and boundary patches from the files and store them appropriately.
+
+        public:
+            int get_num_points(){
+
+            }
+
+            int get_num_faces(){
+
+            }
+
+            int get_num_cells(){
+
+            }
+
+
+};
