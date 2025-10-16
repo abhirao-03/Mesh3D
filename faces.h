e27693bd3b9f3300925cdc1fba328747b2a44f8a
Abhijeet Rao <abhijeetraop@gmail.com>
2025-10-16 20:36:52 +0100

    updated faces.h

--- faces.h
+++ faces.h
@@ -12,14 +12,17 @@ class mesh_faces {
             num_faces = face_connections.size();
         };
 
-        void print_faces(){
-            std::cout << "There are " << num_faces << " faces." << std::endl;
-            for (std::vector<int> faces : face_connections){
-                for (auto i : faces){
-                    std::cout << i << " ";
-                };
-            };
-        };
+        void print_faces() {
+            std::cout << "There are " << num_faces << " faces:" << std::endl;
+            
+            for (const auto face : face_connections) {
+                std::cout << "( ";
+                for (int point_index : face) {
+                    std::cout << point_index << " ";
+                }
+                std::cout << ")" << std::endl;
+            }
+        }
 
         void read_faces_file(std::string file_path);
 };
